61ca64679a42f43e3ee93524a5864ce080188645
Jelaletdin12 <jcarymuhammedow@gmail.com>
2025-02-08 18:26:29 +0500

    fixed style of about us, deliveryTerms

--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import styles from './AboutUs.module.scss';
+import React from "react";
+import styles from "./AboutUs.module.scss";
 
 const AboutUs = () => {
   return (
     <div className={styles.aboutUsContainer}>
-      <h1 className={styles.title}>Sizi Ynamdar onlaýn marketimizde hoş gördiük!</h1>
+      <h1 className={styles.title}>
+        Sizi Ynamdar onlaýn marketimizde hoş gördük!
+      </h1>
       <p className={styles.paragraph}>
-        Onlaýn marketimiz 2019-njy ýyldan bäri hyzmat berýär. Hazirki wagtda Siz ýüzlerçe brendlere degişli bolan münlerçe haryt görüşlerini hödürleýäris! Haryt görüşlerimizi sizden gelyan isleg we teklipirleri esasynda köpeltmäge dowam edýäris. Bizim maksadymyz müşderilerimize ýokary hilli hyzmat bermek bolup durýar. Indi Siz ýüzlerçe sanygdan gerekli zatlar bilen bir hatarda Siz wagtylaryňyz hem ýygytlarsyňyz. Tolegi harytlar size gowşandan soňra nagt ýa-da bank kartlaryňyz üsti bilen amala aşyrýp bilesiňiz!
+        Onlaýn marketimiz 2019-njy ýylyň iýul aýyndan bäri hyzmat berýär.
+        Häzirki wagtda Size ýüzlerçe brendlere degişli bolan müňlerçe haryt
+        görnüşlerini hödürleýäris! Haryt görnüşlerimizi sizden gelýän isleg we
+        teklipleriň esasynda köpeltmäge dowam edýäris. Biziň maksadymyz
+        müşderilerimize ýokary hilli hyzmat bermek bolup durýar. Indi Siz
+        öýüňizden çykmazdan özüňizi gerekli zatlar bilen üpjün edip bilersiňiz!
+        Munuň bilen bir hatarda Siz wagtyňyzy we transport çykdajylaryny hem
+        tygşytlaýarsyňyz. Tölegi harytlar size gowuşandan soňra nagt ýa-da bank
+        kartlarynyň üsti bilen amala aşyryp bilersiňiz!{" "}
       </p>
       <p className={styles.paragraph}>
-        Bizin gapýaryz hyzmatdaşlyklara we teklirlere hemişe açyk!
+        Biziň gapymyz hyzmatdaşlyklara we tekliplere hemişe açyk!
       </p>
     </div>
   );
